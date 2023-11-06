fe925b012da1208ef74c586ed3a35e28caec4c3d
Pascal Schwartz <pascal.schwartz@protonmail.com>
2023-11-06 15:36:49 +0100

    Add descriptions

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -3,5 +3,6 @@ from odoo import fields, models
 
 class EstatePropertyTag(models.Model):
     _name = "estate.property.tag"
+    _description = "Tags for properties"
 
     name = fields.Char()
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,5 +3,6 @@ from odoo import fields, models
 
 class PropertyType(models.Model):
     _name ='estate.property.type'
+    _description = 'Types of properties'
 
     name = fields.Char()
