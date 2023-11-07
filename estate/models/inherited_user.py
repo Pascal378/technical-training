0868925330732f43c0d5615e89f3ebfeee901aa0
Pascal Schwartz <pascal.schwartz@protonmail.com>
2023-11-07 14:30:19 +0100

    Inheritance fix

--- estate/models/inherited_user.py
+++ estate/models/inherited_user.py
@@ -3,7 +3,6 @@ from odoo import fields, models
 
 class InheritedUser(models.Model):
     _inherit = "res.users"
-    _name = "inherited.user"
 
     property_ids = fields.One2many("estate.property", "salesmen_id",
                                    domain="[('state', '!=', 'sold'),('state','!=','cancelled')]")
